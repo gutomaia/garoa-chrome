dc0dd0765cc8d64be09fc56668af8567378e7c0b
Thiago Alvarenga Lechuga <thiagoalz@gmail.com>
2012-03-15 19:30:10 -0700

    Fixing Time. Sorry, my fault.

--- garoa/garoa.js
+++ garoa/garoa.js
@@ -25,7 +25,7 @@ function is_garoa_open(status){
 	console.log("Last Update: " + dateLastchange);
 	
 	//Check lastchange Date
-	var decayTime= 20000; //20 min
+	var decayTime= 20 * 60 * 1000; //20 min
 	if( (new Date().getTime() - (data.lastchange *1000) ) < decayTime ){
 		returnStatus++;
 		strUnknown="";
